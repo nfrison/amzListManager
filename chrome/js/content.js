cc88878ce3577e16c273b201b9dbd8fc8d6728a3
Nicolò <nicolo.frison.1@studenti.unipd.it>
2019-11-24 13:45:24 +0100

    close #6

--- chrome/js/content.js
+++ chrome/js/content.js
@@ -1,48 +1,65 @@
 // content.js
 var pagination;
-var loaded = false;
 var removedProducts = [];
+
+if( window.localStorage.getItem("active") != null ) {
+	removedProducts = JSON.parse( window.localStorage.getItem("removedProducts") );
+	
+	$('.s-result-item').each( function() {
+		if( removedProducts.indexOf( $(this).attr("data-asin") ) != -1 ) {
+			$(this).remove();
+		}
+	});
+	
+	init();
+}
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		if( request.type === "clicked_browser_action" ) {
-			if( !loaded ) {
-				removedProducts = JSON.parse( window.localStorage.getItem('removedProducts') );
-				if( removedProducts == null ) {
-					removedProducts = [];
-				}
+		if( request.type === "activate" ) {
+			if( window.localStorage.getItem("active") == null  ) {
+				removedProducts = [];
+				window.localStorage.setItem("removedProducts", JSON.stringify(removedProducts) );
 				
-				$('.s-result-item').each( function() {
-					if( removedProducts.indexOf( $(this).attr("data-asin") ) != -1 ) {
-						$(this).remove();
-					}
-					$(this).append('<div class="rmRow"></div>');
-				});
+				init();
 				
-				$('.rmRow').on("click", function() {
-					removedProducts.push( $(this).parents('.s-result-item').attr("data-asin") );
-					window.localStorage.setItem('removedProducts', JSON.stringify(removedProducts));
-					$(this).parents('.s-result-item').remove();
-					console.log( removedProducts );
-					console.log( JSON.parse( window.localStorage.getItem('removedProducts') ) );
-				});
-				
-				
-				pagination = $('[data-component-type="s-pagination"]').clone();
-				$('[data-component-type="s-pagination"] .a-text-center').append('<a class="addResults">Carica altri risultati</a>');
-				$('.a-pagination').remove();
-				
-				$('.addResults').on("click",function() {
-					getNextResults();
-				});
-				
-				loaded = true;
+				window.localStorage.setItem("active",true);
 			} else {
-				alert("Amazon Results List Manager già eseguito");
+				alert("Amazon Results List Manager already activated");
+			}
+		} else if( request.type === "reset" ) {
+			if( window.localStorage.getItem("active") != null ) {
+				removedProducts = [];
+				window.localStorage.setItem("removedProducts", JSON.stringify(removedProducts) );
+				alert("Removed products resetted.\nPlease reload your page for these changes to take effect");
+			} else {
+				alert("Amazon Results List Manager not activated");
 			}
 		}
 	}
 );
+
+function init() {
+	// append "rmRow" in the end of each ".s-result-item"
+	$('.s-result-item').each( function() {
+		$(this).append('<div class="rmRow"></div>');
+	});
+	
+	$('.rmRow').on("click", function() {
+		removedProducts.push( $(this).parents('.s-result-item').attr("data-asin") );
+		window.localStorage.setItem('removedProducts', JSON.stringify(removedProducts));
+		$(this).parents('.s-result-item').remove();
+	});
+	
+	// replace "change page" with "Load more results"
+	pagination = $('[data-component-type="s-pagination"]').clone();
+	$('[data-component-type="s-pagination"] .a-text-center').append('<a class="addResults">Load more results</a>');
+	$('.a-pagination').remove();
+	
+	$('.addResults').on("click",function() {
+		getNextResults();
+	});
+}
 	
 function getNextResults() {
 	var nextPageUrl = pagination.find('.a-selected').next('.a-normal').find('a').attr("href");
@@ -55,8 +72,12 @@ function getNextResults() {
 			nextPage = $(data);
 			
 			nextPage.find('.s-result-item').each(function() {
-				$(this).append('<div class="rmRow"></div>');
-				$('.s-result-list').append($(this));
+				if( removedProducts.indexOf( $(this).attr("data-asin") ) != -1 ) {
+					$(this).remove();
+				} else {
+					$(this).append('<div class="rmRow"></div>');
+					$('.s-result-list').append($(this));
+				}
 			});
 			
 			$('.rmRow').on("click", function() {
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -1,10 +1,18 @@
 window.onload = function() {
 	document.getElementById('activate').addEventListener('click', activate);
+	document.getElementById('reset').addEventListener('click', reset);
 }
 
 function activate() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"type": "clicked_browser_action"});
+		chrome.tabs.sendMessage(activeTab.id, {"type": "activate"});
+	});
+}
+
+function reset() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var activeTab = tabs[0];
+		chrome.tabs.sendMessage(activeTab.id, {"type": "reset"});
 	});
 }
